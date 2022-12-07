cdc70754ffffe3ef31f78bdc5b3d409cc8c26365
abhijit424515 <abhijit.amrendra.kumar@gmail.com>
2022-12-07 23:39:54 +0530

    fixed static assets issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
+import qr from "./assets/images/image-qr-code.png";
+
 export default function App() {
 	return (
 		<div className="w-screen h-screen flex justify-center items-center">
 			<div className="h-3/5 bg-[#ffffff] p-[1rem] pb-[1.5rem] rounded-[1rem] aspect-[0.667] shadow-lg">
 				<div className="h-2/3 w-full">
-					<img
-						src="../src/assets/images/image-qr-code.png"
-						className="rounded-[1rem]"
-					/>
+					<img src={qr} className="rounded-[1rem]" />
 				</div>
 				<div className="w-full h-1/3 px-[1rem] flex flex-col justify-end">
 					<p className="text-[#1f3251] font-outfit font-[700] text-center py-[0.5rem] leading-7 h-full text-[2.5vh] flex items-center">
